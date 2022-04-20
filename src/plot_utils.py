b71f5220f3bf725ec470f3546e025fb7e6003ab3
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-04-20 11:11:03 -0400

    Plot utils enabled

---
+++ src/plot_utils.py
@@ -0,0 +1,40 @@
+from textstat import textstat
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_fre_acc_graph_for_model(model, dataset, save_path='./img/result.jpg', min_fre=40, max_fre=100, step_size=5):
+    data = []
+    for example in dataset:
+        fre_score = textstat.flesch_reading_ease(example['article'])
+        logits = model(dataset.collate_fn([example])).logits.unsqueeze()
+        pred = logits.argmax(-1)
+        result = True if pred.item == example['label'] else False
+        data.append((fre_score, result))
+
+    data.sort(key=lambda x: x[0])
+
+    x, y = [], []
+
+    bound = (min_fre, min_fre+step_size)
+    cur_tot, cur_correct = 0, 0
+    i = 0
+    while i < len(data):
+        f, r = data[i]
+        if f > bound[1]:
+            bound = (bound[0]+step_size, bound[1]+step_size)
+            if cur_tot:
+                x.append(sum(bound)/2)
+                y.append(cur_correct/cur_tot)
+            cur_tot, cur_correct = 0, 0
+        else:
+            cur_tot += 1
+            cur_correct += int(r)
+
+    plt.plot(x, y)
+    plt.xticks(np.xticks(np.arange(min_fre, max_fre, step_size)))
+    plt.savefig(save_path)
+
+
+
+
+
