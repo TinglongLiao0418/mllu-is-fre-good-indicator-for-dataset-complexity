1cc2ddcd004239b98056d1293efd84a74e20d679
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-05-04 15:13:58 -0400

    Update prediction result script

--- experiment/bert-base-uncased/predict.py
+++ experiment/bert-base-uncased/predict.py
@@ -2,7 +2,9 @@ import sys
 project_path = '/'.join(sys.path[0].split('/')[:sys.path[0].split('/').index('mllu-is-fre-good-indicator-for-dataset-complexity')+1])
 sys.path.insert(1, project_path)
 
-from src.plot_utils import plot_fre_acc_graph_for_model
+from tqdm import tqdm
+import pandas as pd
+from textstat import textstat
 from transformers import BertTokenizer, BertForMultipleChoice
 from src.dataset import RACEDataset
 
@@ -11,5 +13,13 @@ if __name__ == '__main__':
     test_dataset = RACEDataset(path="../../data/RACE",
                                tokenizer=tokenizer, split_type='test')
 
+    df = pd.DataFrame(columns=['fre_score', 'is_right_prediction'])
     model = BertForMultipleChoice.from_pretrained('log/checkpoint-65000')
-    plot_fre_acc_graph_for_model(model, test_dataset, save_path='fig/result.jpg')
+    for example in tqdm(test_dataset):
+        fre_score = textstat.flesch_reading_ease(example['article'])
+        logits = model(**test_dataset.collate_fn([example])).logits.squeeze()
+        pred = logits.argmax(-1)
+        result = 1 if pred.item() == example['label'] else 0
+        df.loc[len(df.index)] = {'fre_score': fre_score, 'is_right_prediction': result}
+
+    df.to_csv('prediction_fre.csv')
